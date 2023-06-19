e48de782b443d00d851cd8026d37660736bc4208
maor hornstein <mor.h100@gmail.com>
2023-06-19 15:38:52 +0300

    fix incorrect sequence padding (padding needs to be after creating negative samples and not before)
    We need that the padding will happen after creating the negative samples.
    Otherwise, we'll add a lot of redundent 'N's to the negative samples.

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def get_file_number(file_path):
         numeric_value = re.search(r'_(\d+)n', file_name).group(1)
         return int(numeric_value)
 
-def read_samples(file_path, num_of_samples, max_sample_length, padding_char):
+def read_samples(file_path, num_of_samples):
     lines = []
     count = 0
     with open(file_path, 'r') as f:
@@ -45,12 +45,11 @@ def read_samples(file_path, num_of_samples, max_sample_length, padding_char):
             if count >= num_of_samples:
                 break
             seq = line.strip().split()[0]
-            seq = seq[:max_sample_length] if len(seq) > max_sample_length else seq.ljust(max_sample_length, padding_char)
             lines.append(seq)
             count += 1
     return lines
 
-def create_positive_dataset(mode, files, set_size, max_sample_length, padding_char):
+def create_positive_dataset(files, mode, set_size):
     dataset = []
     filesnames = sorted(files, key=get_file_number)
     total_consentrations = sum([get_file_number(file_path) for file_path in files])
@@ -59,7 +58,7 @@ def create_positive_dataset(mode, files, set_size, max_sample_length, padding_ch
             consentration = get_file_number(filename)
             percentage = consentration / total_consentrations
             num_of_samples = set_size * percentage
-            lines = read_samples(filename, num_of_samples, max_sample_length, padding_char)
+            lines = read_samples(filename, num_of_samples)
             dataset += lines
     else:
         raise ValueError(f'Unknown mode: {mode}')
@@ -73,6 +72,10 @@ def shuffle_samples(samples):
 def encode_sequence_list(sequence_list, encoding):
     encoded_sequences = [np.array([encoding[base] for base in sequence]) for sequence in sequence_list]
     return np.array(encoded_sequences)
+
+def pad_samples(samples, max_length, padding_char):
+    padded_samples = list(map(lambda s: s[:max_length].ljust(max_length, padding_char), samples))
+    return padded_samples
 
 def create_model(input_shape, filters, kernel_size, pool_size, hidden_layers_dims):
     padding_shape = (kernel_size[0] - 1, kernel_size[1])
@@ -98,11 +101,14 @@ if __name__ == '__main__':
     rna_compete_sequences = load_rna_compete(rna_compete_filename)
     
     rbns_files = sys.argv[2:]
-    positive_samples = create_positive_dataset(MODE, rbns_files, SET_SIZE, MAX_SAMPLE_LENGTH, PADDING_CHAR)
+    positive_samples = create_positive_dataset(rbns_files, MODE, SET_SIZE)
     negative_samples = shuffle_samples(positive_samples)
 
+    positive_samples = pad_samples(positive_samples, MAX_SAMPLE_LENGTH, PADDING_CHAR)
+    negative_samples = pad_samples(negative_samples, MAX_SAMPLE_LENGTH, PADDING_CHAR)
+
     positive_labels = [1] * len(positive_samples)
-    negative_labels = [0] * len(positive_samples)
+    negative_labels = [0] * len(negative_samples)
 
     samples = encode_sequence_list(positive_samples + negative_samples, ENCODING)
     labels = np.array(positive_labels + negative_labels).reshape(-1, 1)
@@ -110,7 +116,6 @@ if __name__ == '__main__':
     # shuffling the data
     index = np.arange(samples.shape[0]) # Create an index array and shuffle it
     np.random.shuffle(index)
-
     samples = samples[index]
     labels = labels[index]
 
