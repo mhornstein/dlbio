172c798a78b3776958ec466fd76903fae84d6648
maor hornstein <mor.h100@gmail.com>
2023-06-23 10:20:10 +0300

    Adapt encoding process so the tensor will be created only once.
    It made it faster:
    For 1 million samples:
    Before: encoding took ~5 minute.
    After: encoding took ~40 seconds.

--- main.py
+++ main.py
@@ -24,12 +24,12 @@ HIDDEN_LAYERS_DIMS = [128, 64, 1]
 BATCH_SIZE = 64
 
 # Encoding and sequence length constants
-ENCODING = {'A': torch.tensor([1, 0, 0, 0]),
-            'G': torch.tensor([0, 1, 0, 0]),
-            'C': torch.tensor([0, 0, 1, 0]),
-            'U': torch.tensor([0, 0, 0, 1]),
-            'T': torch.tensor([0, 0, 0, 1]),
-            'N': torch.tensor([0.25, 0.25, 0.25, 0.25])}
+ENCODING = {'A': [1, 0, 0, 0],
+            'G': [0, 1, 0, 0],
+            'C': [0, 0, 1, 0],
+            'U': [0, 0, 0, 1],
+            'T': [0, 0, 0, 1],
+            'N': [0.25, 0.25, 0.25, 0.25]}
 
 class ConvNet(nn.Module):
     def __init__(self,hidden_layers, pooling_size, dropout_rate):
@@ -122,8 +122,10 @@ def encode_sequence_list(seq_list, encoding):
     encoded_seqs = []
     for seq in seq_list:
         encoded_seq = [encoding[ch] for ch in seq]
-        encoded_seqs.append(torch.stack(encoded_seq).t())
-    return torch.stack(encoded_seqs)  # We stack the list to create a single tensor
+        encoded_seqs.append(encoded_seq)
+    tensor = torch.tensor(encoded_seqs)
+    tensor = torch.transpose(tensor, -1, -2) # transpose: SET_SIZE X MAX_SAMPLE_LENGTH X 4 => SET_SIZE X 4 X MAX_SAMPLE_LENGTH
+    return tensor
 
 def pad_samples(samples, max_length, padding_char):
     padded_samples = list(map(lambda s: s[:max_length].ljust(max_length, padding_char), samples))
