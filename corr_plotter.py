1f0c89b308fcee87595feab03f19547d165f205c
maor hornstein <mor.h100@gmail.com>
2023-07-17 12:54:07 +0300

    Add correlation plotting - commit 2/2

--- corr_plotter.py
+++ corr_plotter.py
@@ -1,10 +1,8 @@
 import os
 from data_util import load_intensities_file
 from scipy.stats import pearsonr
-
-def get_rbns_name(filename):
-    end_index = filename.index(".")
-    return filename[:end_index]
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def get_rbns_number(filename):
     start_index = filename.index("RBP") + 3  # Adding 3 to skip "RBP"
@@ -12,18 +10,44 @@ def get_rbns_number(filename):
     number = int(filename[start_index:end_index])
     return number
 
+'''
+This script is used to calculate the correlation between the gold and predicted scores of the RNCMPT training data.
+The script requires the following inputs:
+
+train_gold_dir: A string representing the directory path where the gold (actual) scores for the RNCMPT training data are stored.
+It should contain files of the form RBP1.txt...RBP16.txt.
+
+train_predictions_dir: A string representing the directory path where the predicted scores for the RNCMPT training data are stored. 
+These predicted scores are the output of the model's performance on the training data. They can be created using the tester.py script.
+It should contain files of the same form as train_gold_dir, i.e. RBP1.txt...RBP16.txt.
+
+results_dir: The directory where the results should be saved.
+The result will contain a CSV containing the correlation scores and a corresponding box plot.
+'''
 if __name__ == '__main__':
     train_gold_dir = 'data/RNCMPT_training'
     train_predictions_dir = 'train_set_results'
+    results_dir = 'corr_report'
+
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
 
     files_list = os.listdir(train_gold_dir)
 
-    for file in files_list:
-        rbns_number = get_rbns_number(file)
-        rbns_name = get_rbns_name(file)
-        gold_intensities = load_intensities_file(f'{train_gold_dir}/{file}')
-        predicted_intensities = load_intensities_file(f'{train_predictions_dir}/{file}')
-        corr, _ = pearsonr(gold_intensities, predicted_intensities)
-        print(rbns_number, rbns_name, corr)
-    print()
+    # create a df with the correlation calculation results
+    data = [(get_rbns_number(file),
+             pearsonr(load_intensities_file(f'{train_gold_dir}/{file}'),
+                      load_intensities_file(f'{train_predictions_dir}/{file}'))[0])
+            for file in files_list]
+    df = pd.DataFrame(data, columns=['rbns_number', 'corr'])
+    df.set_index('rbns_number', inplace=True)  # Set 'rbns_number' as the index
+    df.sort_index(inplace=True)
+
+    df.to_csv(f'{results_dir}/correlation_data.csv')
+
+    plt.boxplot(df['corr'])
+    plt.title('Correlation Box Plot')
+    plt.ylabel('Correlation')
+    plt.savefig(f'{results_dir}/correlation_box_plot.png')
+    plt.close()
 
