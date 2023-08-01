f993a350b40e3716b105f088c973ca7448a07c19
maor hornstein <mor.h100@gmail.com>
2023-08-01 08:47:32 +0300

    move documentation of corr_plotter to begeining of file

--- corr_plotter.py
+++ corr_plotter.py
@@ -1,15 +1,3 @@
-import os
-from data_util import load_intensities_file
-from scipy.stats import pearsonr
-import pandas as pd
-import matplotlib.pyplot as plt
-
-def get_rbns_number(filename):
-    start_index = filename.index("RBP") + 3  # Adding 3 to skip "RBP"
-    end_index = filename.index(".")
-    number = int(filename[start_index:end_index])
-    return number
-
 '''
 This script is used to calculate the correlation between the gold and predicted scores of the RNCMPT training data.
 The script requires the following inputs:
@@ -17,13 +5,29 @@ The script requires the following inputs:
 train_gold_dir: A string representing the directory path where the gold (actual) scores for the RNCMPT training data are stored.
 It should contain files of the form RBP1.txt...RBP16.txt.
 
-train_predictions_dir: A string representing the directory path where the predicted scores for the RNCMPT training data are stored. 
+train_predictions_dir: A string representing the directory path where the predicted scores for the RNCMPT training data are stored.
 These predicted scores are the output of the model's performance on the training data. They can be created using the tester.py script.
 It should contain files of the same form as train_gold_dir, i.e. RBP1.txt...RBP16.txt.
 
 results_dir: The directory where the results should be saved.
 The result will contain a CSV containing the correlation scores and a corresponding box plot.
 '''
+
+import os
+from data_util import load_intensities_file
+from scipy.stats import pearsonr
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def get_rbns_number(filename):
+    '''
+    Extracts RBP number from filename
+    '''
+    start_index = filename.index("RBP") + 3  # Adding 3 to skip "RBP"
+    end_index = filename.index(".")
+    number = int(filename[start_index:end_index])
+    return number
+
 if __name__ == '__main__':
     train_gold_dir = 'data/RNCMPT_training'
     train_predictions_dir = 'train_set_results'
