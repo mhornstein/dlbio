fb32494aaf1d2feb9c1e70b277ec05ebcef417b0
maor hornstein <mor.h100@gmail.com>
2023-08-01 08:59:52 +0300

    Add documentation

--- encoding_util.py
+++ encoding_util.py
@@ -1,3 +1,7 @@
+'''
+This utility script provides the functionality of encoding the RNA sequences as ordinal numbers or continous vectors
+'''
+
 import torch
 import torch.nn as nn
 
@@ -14,6 +18,18 @@ ONE_HOT_ENCODING = { 'N': [0.25, 0.25, 0.25, 0.25],
                      'T': [0, 0, 0, 1] }
 
 def get_embedding_layer(embedding_dim):
+    '''
+    Creates an embedding layer used for representing nucleotide sequences.
+    It supports two types of embedding layers: "one-hot encoding" for untrainable (frozen) representations
+    and "trainable embeddings" for continuous numerical vectors with a specified dimension.
+
+    Parameters:
+        embedding_dim: The dimensionality of the embedding vectors.
+        Use ONE_HOT for one-hot encoding or any positive integer for trainable embeddings.
+
+    Returns:
+        embeddings (torch.nn.Embedding): The embedding layer object for nucleotide sequence representations.
+    '''
     if embedding_dim == ONE_HOT: # return untrainable (frozen) embedding layer representing the one-hot encoding
         embedding_matrix = torch.FloatTensor([ONE_HOT_ENCODING[nucleotide] for nucleotide in VOCAB])
         embeddings = nn.Embedding.from_pretrained(embedding_matrix, freeze=True)
