dfc02dc833e8c937e074917c9f7cecde3637dd6d
maor hornstein <mor.h100@gmail.com>
2023-08-05 08:22:31 +0300

    Update corr_plotter.py after unix tests

--- corr_plotter.py
+++ corr_plotter.py
@@ -1,20 +1,24 @@
 '''
-This script is used to calculate the correlation between the gold and predicted scores of the RNCMPT training data.
+Small utility script for calculating and visualizing the correlations between RBPs' gold intensities and the predictions.
 
 Usage:
 python corr_plotter.py [path to gold-scores directory] [path to predicted-scores directory] [results directory]
 
 example:
-python corr_plotter.py data\\RNCMPT_training train_set_results corr_report
+python corr_plotter.py data/RNCMPT_training train_set_results corr_report
+where:
+data/RNCMPT_training – was provided as part of the assignment.
+train_set_results – is the output directory of the tester.py script.
 
 Explainations about the parameters:
 * path to gold-scores directory: A string representing the directory path where the gold (actual) scores for the RNCMPT training data are stored.
 It should contain files of the form RBP1.txt...RBP16.txt.
 * path to predicted-scores directory: A string representing the directory path where the predicted scores for the RNCMPT training data are stored.
-These predicted scores are the output of the model's performance on the training data. They can be created using the tester.py script.
-It should contain files of the same form as train_gold_dir, i.e. RBP1.txt...RBP16.txt.
+These predicted scores are the output of the model's predictions for the training data. They can be created using the tester.py script.
+It should contain files of the same form as gold_dir, i.e. RBP1.txt...RBP16.txt.
 * results directory: The directory where the results should be saved.
 
+Results:
 The result will contain a CSV containing the correlation scores and a corresponding box plot.
 '''
 
@@ -23,6 +27,7 @@ from data_util import load_intensities_file
 from scipy.stats import pearsonr
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 
 def get_rbns_number(filename):
     '''
@@ -34,9 +39,13 @@ def get_rbns_number(filename):
     return number
 
 if __name__ == '__main__':
-    train_gold_dir = 'data/RNCMPT_training'
-    train_predictions_dir = 'train_set_results'
-    results_dir = 'corr_report'
+    if len(sys.argv) < 4:
+        print('Some arguments are missing. Please read the README file carefully.')
+        sys.exit(1)
+
+    train_gold_dir = sys.argv[1]
+    train_predictions_dir = sys.argv[2]
+    results_dir = sys.argv[3]
 
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
